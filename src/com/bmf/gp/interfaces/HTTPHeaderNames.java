7bd03560f40a1b91d66034576580bc1b16b61b55
Brendon <bkapitz@gmail.com>
2016-04-12 12:49:45 -0500

    It had to cooperate at some point....mutter...Making progress..Thats what this is

--- src/com/bmf/gp/interfaces/HTTPHeaderNames.java
+++
@@ -1,9 +0,0 @@
-package com.bmf.gp.interfaces;
-
-/**
- * Created by Brendon on 3/31/2016.
- */
-public interface HTTPHeaderNames {
-    String SERVICE_KEY = "service_key";
-    String AUTH_TOKEN = "auth_token";
-}
