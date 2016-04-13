2214ec6eff0298e1e8c73f45bf1019df6bc27141
Felicia <fdurni@madisoncollege.edu>
2016-04-13 18:26:38 -0500

    updated tests for demo

--- tests/com/bmf/gp/persistence/UsersEntityDaoWithHibernateTest.java
+++ tests/com/bmf/gp/persistence/UsersEntityDaoWithHibernateTest.java
@@ -52,7 +52,7 @@ public class UsersEntityDaoWithHibernateTest {
         UsersEntity user = new UsersEntity();
         user.setUserId(1);
         user.setUserName("AtomGirl");
-        user.setPassword("password1");
+        user.setPassword("newpassword1");
         user.setUserRole("admin");
 
         dao.updateUser(user);
@@ -66,7 +66,7 @@ public class UsersEntityDaoWithHibernateTest {
         UsersEntity user = new UsersEntity();
         int sizeBefore;
         int sizeAfter;
-        user.setUserId(26);
+        user.setUserId(35);
         sizeBefore = dao.getAllUsers().size();
         dao.deleteUser(user);
         sizeAfter = dao.getAllUsers().size();
